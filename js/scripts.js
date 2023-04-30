299e5c1f87507855a065b156ea34c2d4c7f6ce8e
Tanmoy Ganguly <xanmoy@outlook.com>
2023-04-30 13:53:25 +0530

    deployed

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,24 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+var cursor = true;
+var speed = 220;
+
+setInterval(() => {
+   if(cursor) {
+     document.getElementById('cursor').style.opacity = 0;
+     cursor = false;
+   }else {
+     document.getElementById('cursor').style.opacity = 1;
+     cursor = true;
+   }
+}, speed);
+
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
